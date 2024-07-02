b8d18727fe5455b35ebd0622e33d28a0b4711cfe
mirkocaforio <mirkocaforio2002@gmail.com>
2024-07-02 09:21:38 +0200

    Fix SettingController

--- src/main/java/it/unisalento/pasproject/paymentservice/business/PaymentConstants.java
+++
@@ -1,7 +0,0 @@
-package it.unisalento.pasproject.paymentservice.business;
-
-public class PaymentConstants {
-    public static final double MEDIUM_ENERGY_COST = 0.12209;
-    public static final float MEDIUM_RESOURCE_CONSUMPTION = 0.3F;
-    public static final float CHANGE_CONSTANT = 0.3F;
-}
--- src/main/java/it/unisalento/pasproject/paymentservice/controller/SettingController.java
+++ src/main/java/it/unisalento/pasproject/paymentservice/controller/SettingController.java
@@ -3,10 +3,13 @@ package it.unisalento.pasproject.paymentservice.controller;
 import it.unisalento.pasproject.paymentservice.dto.SettingDTO;
 import it.unisalento.pasproject.paymentservice.service.CheckOutSetting;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
+import static it.unisalento.pasproject.paymentservice.security.SecurityConstants.ROLE_ADMIN;
+
 @RestController
-@RequestMapping("/api/payment/settings")
+@RequestMapping("/api/settings/payment")
 public class SettingController {
     private final CheckOutSetting checkOutSetting;
 
@@ -16,11 +19,13 @@ public class SettingController {
     }
 
     @GetMapping("/get")
+    @Secured(ROLE_ADMIN)
     public SettingDTO getSettings() {
         return checkOutSetting.getSetting();
     }
 
     @PutMapping("/update")
+    @Secured(ROLE_ADMIN)
     public SettingDTO setSettings(@RequestBody SettingDTO settingsDTO) {
         return checkOutSetting.updateSetting(settingsDTO);
     }
