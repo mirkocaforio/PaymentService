f9d7382f7de06698f912aa50c05a61aa99b82481
mirkocaforio <mirkocaforio2002@gmail.com>
2024-07-05 11:10:15 +0200

    Fix di prova

--- src/main/java/it/unisalento/pasproject/paymentservice/RequestManagmentServiceApplication.java
+++ src/main/java/it/unisalento/pasproject/paymentservice/RequestManagmentServiceApplication.java
@@ -2,8 +2,10 @@ package it.unisalento.pasproject.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RequestManagmentServiceApplication {
 
     public static void main(String[] args) {
