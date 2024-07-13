6708a271214b2bd36cffcfaa77e92cec79d941ee
mirkocaforio <mirkocaforio2002@gmail.com>
2024-07-13 17:23:11 +0200

    Fix PaymentController

--- src/main/java/it/unisalento/pasproject/paymentservice/controller/PaymentController.java
+++ src/main/java/it/unisalento/pasproject/paymentservice/controller/PaymentController.java
@@ -3,7 +3,10 @@ package it.unisalento.pasproject.paymentservice.controller;
 import it.unisalento.pasproject.paymentservice.dto.InvoiceDTO;
 import it.unisalento.pasproject.paymentservice.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
+
+import static it.unisalento.pasproject.paymentservice.security.SecurityConstants.ROLE_UTENTE;
 
 @RestController
 @RequestMapping ("/api/payment")
@@ -16,6 +19,7 @@ public class PaymentController {
     }
 
     @PutMapping(value="/pay/{invoiceNumber}")
+    @Secured(ROLE_UTENTE)
     public InvoiceDTO updateInvoice(@PathVariable String invoiceNumber) {
         return paymentService.updateInvoice(invoiceNumber);
     }
